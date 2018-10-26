8eb9896cfe18d9b2e1c5d0205f18ac2110a1f5e1
joneseyit <joneseyit@protonmail.com>
2018-10-26 01:05:30 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def dictionary
+  substitute_hash = {
+  "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+    
+  }
+  
+end
+
+#use has.key - see if it's true 
+#then use fetch to pop value
+#build new array
+# add to new array
+def word_substituter(tweet)
+  new_array = []
+  tweet_words = tweet.split
+  tweet_words.each do |word|
+  
+  end
+  
+end
